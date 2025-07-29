935c23c36baa871696a34f630f4fe1b9486e347e
David Offei Akuffo <david.akuffo11@gmail.com>
2025-07-29 12:28:46 +0000

    routes modification

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,23 @@
+
 import React from 'react'
+import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
+import Hero from '../components/Hero';
+import About from '../pages/About';
+import Services from '../pages/Services';
+import Contact from '../pages/Contact';
+
 
 const Routes = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <Router>
+      <Switch>
+        <Route path="/" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default Routes
